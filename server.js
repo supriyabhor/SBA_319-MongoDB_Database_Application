1c11ddd39262af8bee8357bb0e4f1c74f774d996
supriyabhor <supriyanaikwadi89@gmail.com>
2025-02-03 02:21:39 -0500

    post method working

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ require('dotenv').config();
 
 const store = require("./models/customer.js");
 
-const store1=require ("./models/product.js")
 
 mongoose.connect(process.env.MONGODB_URI)
 mongoose.connection.on("connected", () => {
@@ -21,12 +20,40 @@ app.get("/", async (req, res) => {
     res.send("welcome")
 });
 
-
+//GET
 app.get("/customers", async (req, res) => {
     const allCustomer = await customer.find();
      console.log(allCustomer);
     res.send(allCustomer);
 });
+
+//POST
+app.post("/customers", async (req, res) => {
+    try {
+        console.log("Received data:", req.body); // Log the incoming request data
+
+        const newCustomer = new customer({
+            name: req.body.name,
+            email: req.body.email,
+            address: req.body.address,
+            phone: req.body.phone,
+        });
+
+        await newCustomer.save();
+
+        console.log("New customer created:", newCustomer); // Log the created customer
+        res.status(201).send(newCustomer); // Send the created customer back
+    } catch (error) {
+        console.error("Error creating customer:", error.message); // Log error
+        res.status(400).send("Error creating customer: " + error.message);
+    }
+});
+
+//
+
+
+
+
 
 
 
